6d11fd4fcacb6b275b986217055da9981e0adb86
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-04-25 03:12:51 +0100

    Cooking...

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -3,11 +3,54 @@ import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import ProgressiveImage from "react-progressive-image";
 
+import Khalidat from "../Images/khalidah.jpg"
+
 const transition = { duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96] };
 
-const Home = ({ imageDetails, image }) => {
+const Home = ({ ImageDetails, image }) => {
     return (
-        <div>Home</div>
+        <main>
+            <div className='container'>
+                <div className='row center'>
+                    <div className='image-container'>
+                        <div
+                            className='thumbnail'
+                            ref={image}
+                            style={{
+                                width: ImageDetails.width,
+                                height: ImageDetails.height,
+                            }}>
+                            <div className='frame'>
+                                <Link to={`/Khalidat`}>
+                                    <ProgressiveImage
+                                        src={`${Khalidat}`}
+                                        placeholder='Khalidat Sulyman'>
+                                        {(src) => (
+                                            <motion.img
+                                                src={src}
+                                                alt='Khalidat Sulyman'
+                                                whileHover={{ scale: 1.1 }}
+                                                transition={transition}
+                                            />
+                                        )}
+                                    </ProgressiveImage>
+                                </Link>
+                            </div>
+                        </div>
+                        <motion.div
+                            exit={{ opacity: 0 }}
+                            transition={transition}
+                            className='information'>
+                            <div className='title'>Khalidat Sulyman</div>
+                            <div className='location'>
+                                <span>28.538336</span>
+                                <span>-81.379234</span>
+                            </div>
+                        </motion.div>
+                    </div>
+                </div>
+            </div>
+        </main>
     )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
+import "./Styles/Index.css"
+import "./Styles/Index.scss"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
